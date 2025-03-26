f578bab1aa6d0d41ac883314697528fa87b2cbe5
tlebryk <tlebryk02@gmail.com>
2025-03-25 22:36:30 -0400

    remove the os call

--- src/replica_server.py
+++ src/replica_server.py
@@ -61,7 +61,7 @@ def transition_to_leader_mode(replica_addresses):
 
     logger.info(f"Executing leader mode with command: {new_args}")
 
-    subprocess.Popen(new_args, preexec_fn=os.setpgrp)
+    subprocess.Popen(new_args)
     # Exit the current process after starting the new leader.
     os._exit(0)
 
